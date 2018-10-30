47ec95f22a6646d848b1e6b149353850a845e99d
Vladislav <vladdislav.georgiev@gmail.com>
2018-10-30 23:49:29 +0200

    example something

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = new express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+ 
+var port = process.env.port || 3000;
+ 
+app.use(express.static(__dirname + "/public" ));
+ 
+app.get('/',function(req,res){
+    res.redirect('index.html');
+});
+ 
+ 
+io.on('connection',function(socket){
+ 
+    socket.on('stream',function(image){
+        socket.broadcast.emit('stream',image);  
+    });
+ 
+});
+ 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
